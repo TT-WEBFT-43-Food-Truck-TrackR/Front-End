3b203bd0bd71e787a5f56e415acf635bcd33de19
Andrew Poppenberg <andrewpoppenberg@Andrews-MacBook-Pro.local>
2020-10-21 15:46:53 -0500

    finished styling add truck form

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,52 @@
 import React, { useState } from 'react'
 import { NavLink, Route, useHistory, useRouteMatch, useParams } from "react-router-dom"
 import { v4 as uuid } from "uuid"
-
+import styled from 'styled-components'
 import AddTruckForm from "../AddTruckForm/AddTruckForm"
 import Truck from "../Truck/Truck"
+
+
+
+const StyledTrucks = styled.div`
+display:flex;
+box-shadow:0.5rem 1rem 0.8rem grey;
+background-color:#4D4D4D;
+width:50%;
+border:2px solid red;
+margin:1% auto;
+flex-direction:column;
+font-size:2rem;
+padding:1%;
+align-items:center;
+text-align:center;
+button:hover{
+  color:#0392A6;
+}
+button:active{
+  border-radius:8%;
+  transform:scale(.94);
+}
+button{
+  border:1px solid black;
+  font-size:calc(2rem + 0.4vw);
+  text-align:center;
+}
+h2{
+  font-size:calc(3rem + 0.4vw);
+}
+h3{
+  font-size:calc(2rem + 0.4vw);
+}
+h4{
+  font-size:calc(1.7rem + 0.4vw);
+}
+`
+const StyledPage = styled.div`
+text-align:center; 
+h1{
+  font-size:5rem;
+}
+`
 
 export default function Dashboard() {
   const [user, setUser] = useState({ id: uuid(), username: "chrisg", email: "cg@cg.com", trucks: [
@@ -36,28 +79,27 @@ export default function Dashboard() {
   }
 
   return (
-    <div className="user-dashboard">
-      <div className="user-dashboard-username">Welcome back, { user.username }</div>
-      <div className="user-dashboard-trucks">
+    <StyledPage>
+      <div className="user-dashboard-username"><h1>Welcome back, {user.username}</h1></div>
+      <div>
       { user.trucks.map(truck => {
         return (
-          <>
-            <div className="user-dashboard-truck-name">Truck Name: { truck.truckName }</div>
-            <div className="user-dashboard-truck-location">Location: { truck.location }</div>
-            <div className="user-dashboard-truck-">Category: { truck.category }</div>
+          <StyledTrucks>
+            <div className="user-dashboard-truck-name"><h2>{truck.truckName}</h2></div>
+            <div className="user-dashboard-truck-location"><h3>{truck.location}</h3></div>
+            <div className="user-dashboard-truck-"><h4>Catagory: {truck.category}</h4></div>
             <button onClick={() => editTruck(truck.truckName) }>Edit Truck</button>
-          </>
+          </StyledTrucks>
         )
       })}
       </div>
       <div className="user-dash-board-add-truck-form">
-        <h2>Add Truck</h2>
         <AddTruckForm addTruck={ addTruck } />
       </div>
 
       <Route path="/dashboard/:truckName">
         <Truck />
       </Route>
-    </div>
+    </StyledPage>
   )
 }
