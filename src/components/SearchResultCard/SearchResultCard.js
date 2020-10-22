7cbad290eacb2070228e444e7dbfae2cd28309c6
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-10-22 01:53:28 -0400

    add review works in state and in the post request

--- src/components/SearchResultCard/SearchResultCard.js
+++ src/components/SearchResultCard/SearchResultCard.js
@@ -1,6 +1,8 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { v4 as uuid } from "uuid"
 import styled from 'styled-components'
+
+import { axiosWithAuth } from "../../utils/axiosWithAuth"
 
 // { id: uuid(), truckName: "Mexican Truck Food", location: "Los Angeles, California", reviews: [
 //   {id: uuid(), rating: 3, comment: "This food was good"},
@@ -114,15 +116,36 @@ export default function SearchResultCard({ result, addRating }) {
   const [expandThings, setExpandThings] = useState(false)
   const ratingRef = useRef()
   const commentRef = useRef()
+  const [details, setDetails] = useState(null)
+  const [update, setUpdate] = useState(0)
+
+  useEffect(() => {
+      axiosWithAuth().get(`trucks/${result.id}`)
+        .then(res => {
+            console.log("GET TRUCK SUCCESS ===>", res.data)
+            setDetails(res.data)
+        })
+        .catch(err => console.log("GET TRUCK FAILURE ===>", err))
+    }, [])
+
+  useEffect(() => {
+      axiosWithAuth().get(`trucks/${result.id}`)
+        .then(res => {
+            console.log("GET TRUCK SUCCESS ===>", res.data)
+            setDetails(res.data)
+        })
+        .catch(err => console.log("GET TRUCK FAILURE ===>", err))
+  }, [update])
 
   const onSubmit = e => {
     e.preventDefault()
     e.stopPropagation()
     const stars = ratingRef.current.value
     const feedback = commentRef.current.value
-    addRating({ id: uuid(), rating: stars, comment: feedback }, result)
+    addRating({ rating: stars, comment: feedback, truck_id: result.id})
     ratingRef.current.value = ''
     commentRef.current.value = ''
+    setUpdate(update + 1)
   }
   const seeReviews = e => {
     setExpandReviews(!expandReviews)
@@ -142,11 +165,15 @@ export default function SearchResultCard({ result, addRating }) {
     e.stopPropagation()
     setExpandThings(!expandThings)
   }
+  
+  if (!details) {
+    return <div>Loading results...</div>
+  }
   return (
     <StyledCard>
-      <div className="search-result-card-name"><h2>{result.name}</h2></div>
-      <div className="search-result-card-location"><h3>{result.city}</h3></div>
-      <div className="search-result-card-cuisine"><h3>{result.cuisine}</h3></div>
+      <div className="search-result-card-name"><h2>{details.name}</h2></div>
+      <div className="search-result-card-location"><h3>{details.city}</h3></div>
+      <div className="search-result-card-cuisine"><h3>{details.cuisine}</h3></div>
       {/* <div className="search-result-card-rating-avg"><h3>Rating: {result.ratingAvg}</h3>
       </div> */}
       <div className={expandThings === true ? 'dissappear' : ''} onClick={seeThings}><span>v</span></div>
@@ -158,7 +185,7 @@ export default function SearchResultCard({ result, addRating }) {
             {
               expandMenu &&
 
-              result.menu.map(item => {
+              details.menu.map(item => {
                   return (
                     <div className="toLeft">
                       <div><h3>Dish</h3> <p>{item.name}</p></div>
@@ -172,7 +199,7 @@ export default function SearchResultCard({ result, addRating }) {
             {
               expandReviews &&
 
-              result.reviews.map(review => {
+              details.reviews.map(review => {
                   return (
                     <div className="toLeft">
                       <div><h3>Username</h3> <p>{review.username}</p></div>
